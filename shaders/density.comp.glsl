c79e99461788083175432b6a9a09234f2ef6c9e4
Namit Bhutani <namit.bhut@gmail.com>
2025-11-23 01:29:52 +0530

    feat: use FastNoiseLite to generate 2.5D surface

--- shaders/density.comp.glsl
+++ shaders/density.comp.glsl
@@ -1,4 +1,3 @@
-// sphere sdf
 #version 460 core
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 
@@ -7,18 +6,31 @@ layout(std430, binding = 0) buffer DensityBuffer {
 };
 
 uniform int gridSize;
+uniform int u_Seed;
+uniform vec3 u_Offset;   // Pass chunk offset (e.g., vec3(0,0,0))
 
 void main() {
-    uvec3 id = gl_GlobalInvocationID;
+    uvec3 id = gl_GlobalInvocationID.xyz;
     if (id.x >= gridSize || id.y >= gridSize || id.z >= gridSize) return;
 
     uint index = id.x + id.y * gridSize + id.z * gridSize * gridSize;
-    vec3 pos = vec3(id);
     
-    vec3 center = vec3(gridSize) * 0.5;
-    float radius = float(gridSize) * 0.3;
-    
-    float val = length(pos - center) - radius; 
+    // calc World Position for this voxel
+    vec3 worldPos = vec3(id) + u_Offset;
+
+    // setup noise
+    fnl_state noise = fnlCreateState(u_Seed);
+    noise.noise_type = FNL_NOISE_OPENSIMPLEX2;
+    noise.fractal_type = FNL_FRACTAL_FBM;
+    noise.frequency = 0.02; 
+    noise.octaves = 4;
+
+    // 2d noise
+    float terrainHeight = fnlGetNoise3D(noise, worldPos.x, 0.0, worldPos.z) * 20.0 + 10.0;
+
+    float val = terrainHeight - worldPos.y;
+
+    // todo: sdf cave system goes here
 
     density[index] = val;
 }
